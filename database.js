f6fe7223f795181307feb3bc142a399a72fa9690
Merve Erdol <merve-uygun@windowslive.com>
2017-10-25 10:54:49 +0300

    Read data from data.json

--- database.js
+++ database.js
@@ -3,3 +3,5 @@ const fs = require('fs');
 exports.save = (people) => {
   fs.writeFileSync('./data.json', JSON.stringify(people));
 };
+
+exports.load = () => JSON.parse(fs.readFileSync('./data.json', 'utf8'));
--- index.js
+++ index.js
@@ -10,34 +10,38 @@ const firstStudent = new Student('Merve', 29, 'Ankara');
 const secondStudent = new Student('Gulistan', 27, 'London');
 const thirdStudent = new Student('Kubra', 25, 'Istanbul');
 
-//firstStudent.makeFriendsWith(secondStudent);
+// firstStudent.makeFriendsWith(secondStudent);
 
 const students = [firstStudent, secondStudent, thirdStudent];
-Database.save(students);
 
 const firstCourse = new Course('JS', 30, 'Armagan');
 const secondCourse = new Course('CSS', 20, 'Yaprak');
 
 const courses = [firstCourse, secondCourse];
 
-console.log(firstCourse);
+/* console.log(firstCourse);
 console.log(firstStudent);
 console.log(firstStudent.name);
-console.log(`${secondStudent.name} is from ${secondStudent.city}`);
+console.log(`${secondStudent.name} is from ${secondStudent.city}`); */
 
-addAges(firstStudent.age, thirdStudent.age);
-whichCity(secondStudent.name, secondStudent.city);
+// addAges(firstStudent.age, thirdStudent.age);
+// whichCity(secondStudent.name, secondStudent.city);
 
-whichCity(firstStudent.name, firstStudent.city);
+// whichCity(firstStudent.name, firstStudent.city);
 
 firstStudent.interests.push('Javascript', 'NodeJS');
 secondStudent.interests.push('HTML', 'MongoDB');
-console.log(`${firstStudent.name} is interested in ${firstStudent.interests}`);
+/* console.log(`${firstStudent.name} is interested in ${firstStudent.interests}`); */
 
-courses[1].courseDesc();
-firstCourse.courseDesc();
+// courses[1].courseDesc();
+// firstCourse.courseDesc();
 
-const printName = student => student.sayName();
-students.forEach(printName);
+/* const printName = student => student.sayName();
+students.forEach(printName); */
 
-console.log(students[1].friends);
+// console.log(students[1].friends);
+
+Database.save(students);
+
+const loadedFile = Database.load();
+console.log(loadedFile[0].city);
